cac2f0d29b6d7a0d53571eff952ac6e8eb7b087f
kcho2001 <kec623@lehigh.edu>
2022-09-12 14:22:46 -0400

    Got rid of unneeded file

--- navigation/index.js
+++ navigation/index.js
@@ -31,7 +31,6 @@ import Colors from "../constants/Colors";
 import useColorScheme from "../hooks/useColorScheme";
 import ModalScreen from "../screens/ModalScreen";
 import NotFoundScreen from "../screens/NotFoundScreen";
-import TabOneScreen from "../screens/TabOneScreen";
 import TabTwoScreen from "../screens/TabTwoScreen";
 import {
   RootStackParamList,
