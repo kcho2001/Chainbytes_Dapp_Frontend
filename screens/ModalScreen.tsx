0a9856311ebc5b27af08451ccaaf633d1ea2dce6
kcho2001 <kec623@lehigh.edu>
2022-09-12 14:01:31 -0400

    Added functions to allow for logging out in the modal screen. Doesn't work fully yet

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,21 +1,48 @@
+import { useWalletConnect } from '@walletconnect/react-native-dapp';
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import React from 'react';
+import { Platform, StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function ModalScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
+const shortenAddress = (address: string) => {
+  global.myAddress = address;
+  let ret = `${address.slice(0, 6)}...${address.slice(
+    address.length - 4,
+    address.length
+  )}`;
+  return ret;
+}
 
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+export default function ModalScreen() {
+  const connector = useWalletConnect();
+  
+  const killSession = React.useCallback(() => {
+    return connector.killSession();
+  }, [connector]);
+
+  return (
+    <View>
+      {!!connector.connected && (
+        <>
+          <View style={styles.container}>
+            <Text style={styles.title}>Modal</Text>
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            <EditScreenInfo path="/screens/ModalScreen.tsx" />
+            <Text>{shortenAddress(connector.accounts[0])}</Text>
+            <TouchableOpacity onPress={killSession} style={styles.buttonStyle}>
+              <Text style={styles.buttonTextStyle}>Log out</Text>
+            </TouchableOpacity>
+            {/* Use a light status bar on iOS to account for the black space above the modal */}
+            <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+          </View>
+        </>
+      )}
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -31,5 +58,25 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  buttonStyle: {
+    backgroundColor: "#3399FF",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#3399FF",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
