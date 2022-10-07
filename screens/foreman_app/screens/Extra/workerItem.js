1fbdec2804f517d57f8f5f39945682727e155961
kcho2001 <kec623@lehigh.edu>
2022-10-07 17:44:09 -0400

    Added a new type of Item: workerCheckedIn Item

--- screens/foreman_app/screens/Extra/workerItem.js
+++ screens/foreman_app/screens/Extra/workerItem.js
@@ -1,22 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function WorkerItem({ item, pressHandler }) {
-
-    return (
-        <TouchableOpacity onPress = {() => pressHandler(item.text)}>
-            <Text style={styles.item}>{item.text}</Text>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity onPress={() => pressHandler(item.text)}>
+      <Text style={styles.item}>{item.text}</Text>
+    </TouchableOpacity>
+  );
 }
 
+export function WorkerCheckinItem({ item, pressHandler }) {
+  return (
+    <TouchableOpacity onPress={() => pressHandler(item.text)}>
+      <Text style={styles.item}>
+        {shortenAddress(item.id)}: {item.daysUnpaid}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const shortenAddress = (address) => {
+  global.myAddress = address;
+  let ret = `${address.slice(0, 6)}...${address.slice(
+    address.length - 4,
+    address.length
+  )}`;
+  return ret;
+};
+
 const styles = StyleSheet.create({
-    item: {
-        padding: 16,
-        marginTop: 16,
-        borderColor: '#bbb',
-        borderWidth: 1,
-        borderStyle: 'dashed',
-        borderRadius: 10,
-    }
-})
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10,
+  },
+  checkInItem: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10,
+    fontSize: 8,
+  },
+});
