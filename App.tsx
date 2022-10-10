aad90dd0c1fd25ccd52418ae3bc331fdfaf555bf
kcho2001 <kec623@lehigh.edu>
2022-10-10 16:12:05 -0400

    Got rid of unused import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ApolloProvider } from "@apollo/client";
 import useCachedResources from './hooks/useCachedResources';
