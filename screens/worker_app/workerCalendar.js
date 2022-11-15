ecfdee27a01937ae2fc1310b8d21f50a3aaa3727
kcho2001 <kec623@lehigh.edu>
2022-11-15 13:16:17 -0500

    Can now see worker checkins on calendar

--- screens/worker_app/workerCalendar.js
+++ screens/worker_app/workerCalendar.js
@@ -1,8 +1,18 @@
-import { StyleSheet, SafeAreaView, Button } from "react-native";
+import { StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+import Spinner from "react-native-loading-spinner-overlay";
+import { useState } from "react";
 import { Calendar } from "react-native-calendars";
 import * as config from "../ChainBytesConfig";
 import { ethers } from "ethers";
-import { Text, View, backgroundColor, textColor } from '../../components/Themed';
+import {
+  Text,
+  View,
+  backgroundColor,
+  textColor,
+} from "../../components/Themed";
+import * as query from "../../query";
+import { useQuery } from "@apollo/client";
+import { useWalletConnect } from "@walletconnect/react-native-dapp";
 
 // Contract declaration
 const provider = new ethers.providers.JsonRpcProvider(config.providerUrl);
@@ -12,63 +22,129 @@ let contract = new ethers.Contract(
   provider
 );
 
-export default function WorkCalendar(props) {
-  const address = props.address; //Address to use for getting checked in dates
-  let bg = backgroundColor()
-  let tc = textColor()
-
-  
-  return (
-    <SafeAreaView style={[styles.screen, {backgroundColor: bg}]}>
-      <Calendar
-        minDate={"2022-05-01"}
-        hideExtraDays={true}
-        // // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-        //maxDate={}
-        // // Handler which gets executed on day press.
-        // onDayPress={(day) => {
-        //   console.log("selected day", day);
-        // }}
-        enableSwipeMonths={true}
-        markedDates={{ getCheckedIn }}
-        style={{
-          height: '100%',
-          justifyContent: "center",
-          backgroundColor: bg
-        }}
-        theme={{
-          calendarBackground: bg,
-          textSectionTitleColor: tc,
-          todayTextColor: tc,
-          dayTextColor: 'gray',
-          monthTextColor: tc,
-          arrowColor: tc,
-          textDayFontFamily: 'HelveticaNeue-Bold',
-          textMonthFontFamily: 'HelveticaNeue-Bold',
-          textDayHeaderFontFamily: 'HelveticaNeue-Bold',
-          textDayFontSize: 18,
-          textMonthFontSize: 25,
-          textDayHeaderFontSize: 14
-        }}
-      />
-    </SafeAreaView>
-  );
+// Function to format checkIns into marked dates format
+function formatDates(checkIns, setCheckIns) {
+  for (const date of checkIns) {
+    setCheckIns((prevCheckIns) => {
+      const formattedDate = date.year + "-" + date.month + "-" + date.day;
+      return {
+        [formattedDate]: { selectedColor: "#90EE90", selected: true },
+        prevCheckIns,
+      };
+    });
+    console.log(date.year + "-" + date.month + "-" + date.day);
+  }
 }
 
-async function getCheckedIn(address) {
-  const checkedIn = await contract
-    .daysCheckedIn(address)
-    .then((result) => console.log(result));
-  let mark = {};
-  checkedIn.forEach((day) => {
-    mark[day] = { marked: true, dotColor: "#90EE90", activeOpacity: 0 };
-  });
-  return mark;
+export default function WorkCalendar(props) {
+  const connector = useWalletConnect();
+  const [checkIns, setCheckIns] = useState({});
+  let bg = backgroundColor();
+  let tc = textColor();
+
+  const handleRefresh = () => {
+    // manually refetch data
+    refetch();
+  };
+
+  const { loading, error, data, refetch } = useQuery(
+    query.GET_WORKER_CHECKINS(connector.accounts[0]),
+    {
+      onCompleted: () => {
+        // console.log(data.worker.checkIns[0].day);
+        formatDates(data.worker.checkIns, setCheckIns);
+      },
+      notifyOnNetworkStatusChange: true,
+    }
+  );
+
+  return (
+    <>
+      {loading && (
+        <View style={styles.container}>
+          <Spinner
+            visible={loading}
+            textContent={"Loading..."}
+            textStyle={styles.spinnerTextStyle}
+          />
+        </View>
+      )}
+      {error && <Text>Error: {error.message}</Text>}
+      <SafeAreaView style={[styles.screen, { backgroundColor: bg }]}>
+        <Calendar
+          minDate={"2022-05-01"}
+          hideExtraDays={true}
+          // // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+          //maxDate={}
+          // // Handler which gets executed on day press.
+          // onDayPress={(day) => {
+          //   console.log("selected day", day);
+          // }}
+          enableSwipeMonths={true}
+          // markedDates={{ checkIns }}
+          markedDates={checkIns}
+          style={{
+            height: "100%",
+            justifyContent: "center",
+            backgroundColor: bg,
+          }}
+          theme={{
+            calendarBackground: bg,
+            textSectionTitleColor: tc,
+            todayTextColor: tc,
+            dayTextColor: "gray",
+            monthTextColor: tc,
+            arrowColor: tc,
+            textDayFontFamily: "HelveticaNeue-Bold",
+            textMonthFontFamily: "HelveticaNeue-Bold",
+            textDayHeaderFontFamily: "HelveticaNeue-Bold",
+            textDayFontSize: 18,
+            textMonthFontSize: 25,
+            textDayHeaderFontSize: 14,
+          }}
+        />
+        <View style={styles.bottom}>
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={() => handleRefresh()}
+          >
+            <Text style={styles.buttonTextStyle}> Refresh Calendar </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: "center",
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 100,
+  },
+  buttonStyle: {
+    backgroundColor: "#3399FF",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#3399FF",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
