196cb05e1d42b14b26f25b7a05bbad1eb90425b2
kcho2001 <kec623@lehigh.edu>
2022-11-20 17:30:23 -0500

    Can now look at individual balances per person. Also, added in column headers

--- screens/farm_app/batchPay.js
+++ screens/farm_app/batchPay.js
@@ -1,6 +1,6 @@
 import { useWalletConnect } from "@walletconnect/react-native-dapp";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { StyleSheet, FlatList, TouchableOpacity, Image } from "react-native";
+import { StyleSheet, FlatList, TouchableOpacity, Keyboard, TouchableWithoutFeedback } from "react-native";
 import { View, Text, TextInput, textColor } from "../../components/Themed";
 import React, { useEffect, useState, useCallback } from "react";
 import { ethers } from "ethers";
@@ -12,7 +12,7 @@ import { useQuery } from "@apollo/client";
 import * as query from "../../query";
 import moment from "moment";
 import Spinner from "react-native-loading-spinner-overlay";
-import { View, Text, backgroundColor } from '../../components/Themed'
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function BatchPay() {
   //Rate is bound to change depending on how much the workers should be paid (amount in Wei)
@@ -22,12 +22,23 @@ export default function BatchPay() {
   const [balance, setBalance] = useState(0);
   const tc = textColor();
 
+  function textChange(input) {
+    if (input.trim() === '') {
+      setRate(0);
+      return;
+    }
+    if (!isNaN(input)) {
+      setRate(Math.ceil(input));
+    }
+    return;
+  }
+
   // This is to update the balance anytime the workers or rate is changed
   useEffect(() => {
     let _balance = 0;
     for (let worker of workers) {
-      if (worker.workerCheckedIn.daysUnpaid != 0) {
-        _balance += worker.workerCheckedIn.daysUnpaid * rate;
+      if (worker.daysUnpaid != 0) {
+        _balance += worker.daysUnpaid * rate;
       }
     }
     setBalance(_balance);
@@ -56,16 +67,14 @@ export default function BatchPay() {
 
   const { loading, error, data, refetch } = useQuery(query.GET_CHECKINS, {
     onCompleted: () => {
-      setWorkers(data.checkIns);
+      setWorkers(data.workers);
     },
     notifyOnNetworkStatusChange: true,
   });
 
   const removeWorker = (key) => {
     setWorkers((prevWorkers) => {
-      return prevWorkers.filter(
-        (worker) => worker.workerCheckedIn.id != key.id
-      );
+      return prevWorkers.filter((worker) => worker.id != key.id);
     });
   };
 
@@ -96,20 +105,14 @@ export default function BatchPay() {
         let balances = [];
         let balance = 0;
         for (let i = 0; i < workers.length; i++) {
-          if (workers[i].workerCheckedIn.daysUnpaid != 0) {
-            addresses.push(workers[i].workerCheckedIn.id);
-            balances.push(
-              ethers.BigNumber.from(exchangeRate)
-                .mul(workers[i].workerCheckedIn.daysUnpaid)
-                .mul(rate)
-            );
-            balance = ethers.BigNumber.from(balance).add(
-              ethers.BigNumber.from(exchangeRate)
-                .mul(workers[i].workerCheckedIn.daysUnpaid)
-                .mul(rate)
-            );
+          if (workers[i].daysUnpaid != 0) {
+            addresses.push(workers[i].id);
+            let _balance = ethers.BigNumber.from(exchangeRate)
+              .mul(workers[i].daysUnpaid)
+              .mul(rate);
+            balances.push(_balance);
+            balance = ethers.BigNumber.from(balance).add(_balance);
           }
-          console.log(rate);
         }
         // Override to allow a value to be added when calling contract
         let overrides = {
@@ -159,41 +162,132 @@ export default function BatchPay() {
         </View>
       )}
       {!loading && !error && workers.length != 0 && (
-        <View style={styles.container}>
-          <View style={styles.content}>
-            <View style={styles.list}>
-              <FlatList
-                data={workers}
-                keyExtractor={worker => worker.workerCheckedIn.id}
-                renderItem={({ item }) => (
-                  <WorkerCheckinItem
-                    item={{
-                      id: item.workerCheckedIn.id,
-                      daysUnpaid: item.workerCheckedIn.daysUnpaid,
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <View style={styles.container}>
+            <View style={styles.content}>
+              <View style={styles.list}>
+                {/* Header for the list of content */}
+                <View style={{
+                  flexDirection: "row",
+                  alignItems: "baseline"
+                }}>
+                  <Text style={{ flex: 1, textAlign: "left" }}>
+                    Address
+                  </Text>
+                  <Text style={{ flex: 1, textAlign: "center" }}>
+                    Days Unpaid
+                  </Text>
+                  <Text style={{ flex: 1, textAlign: "right" }}>
+                    Balance
+                  </Text>
+                </View>
+                <FlatList
+                  data={workers}
+                  renderItem={({ item }) => (
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "baseline"
+                      }}
+                    >
+                      <View style={{ flex: 1, flexGrow: 3 }}>
+                        <WorkerCheckinItem
+                          style={{
+                            position: "relative",
+                          }}
+                          item={{
+                            id: item.id,
+                            daysUnpaid: item.daysUnpaid,
+                          }}
+                          pressHandler={removeWorker}
+                          keyExtractor={(item, index) => index.toString()}
+                        ></WorkerCheckinItem>
+                      </View>
+                      <View style={{ flex: 1, flexGrow: 2 }}>
+                        <Text
+                          style={{
+                            position: "relative",
+                            alignSelf: "flex-end",
+                          }}
+                        >
+                          ${item.daysUnpaid * rate}
+                        </Text>
+                      </View>
+                    </View>
+                  )}
+                ></FlatList>
+              </View>
+            </View>
+
+            <View style={styles.bottom}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  textAlignVertical: "center",
+                }}
+              >
+                <Text>Total balance:{"     "}</Text>
+                <View style={{ flexDirection: "column" }}>
+                  <Text
+                    style={{
+                      alignSelf: "flex-start",
+                      textAlignVertical: "top",
+                      position: "relative",
                     }}
-                    pressHandler={removeWorker}
-                    keyExtractor={(item, index) => index.toString()}
-                  ></WorkerCheckinItem>
-                )}
-              ></FlatList>
+                  >
+                    ~${balance.toString()}
+                  </Text>
+                  <Text
+                    style={{
+                      textAlignVertical: "bottom",
+                      position: "relative",
+                      alignSelf: "flex-start",
+                    }}
+                  >
+                    {ethers.utils
+                      .formatEther(
+                        ethers.BigNumber.from(balance)
+                          .mul(exchangeRate)
+                          .toString()
+                      )
+                      .slice(0, 7)}{" "}
+                    ETH
+                  </Text>
+                </View>
+              </View>
+
+              <View>
+                <Text style={{ padding: 15, paddingBottom: 5 }}>
+                  Rate to pay workers (USD per day)
+                </Text>
+                <View>
+                  <TextInput
+                    style={[styles.input, { borderColor: tc }, { color: tc }]}
+                    keyboardType='numeric'
+                    onChangeText={textChange}
+                    placeholder="Rate"
+                    value={rate != 0 || rate != "" ? rate : ""}
+                    placeholderTextColor="grey"
+                  />
+                </View>
+              </View>
+              <TouchableOpacity
+                style={styles.buttonStyle}
+                onPress={() => handleRefresh()}
+              >
+                <Text style={styles.buttonTextStyle}> Refresh List </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.buttonStyle}
+                onPress={() => batchPay(rate, workers)}
+              >
+                <Text style={styles.buttonTextStyle}> Pay workers </Text>
+              </TouchableOpacity>
             </View>
           </View>
-          <View style={styles.bottom}>
-            <TouchableOpacity
-              style={styles.buttonStyle}
-              onPress={() => handleRefresh()}
-            >
-              <Text style={styles.buttonTextStyle}> Refresh List </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.buttonStyle}
-              onPress={() => batchPay(rate, workers)}
-            >
-              <Text style={styles.buttonTextStyle}> Pay workers </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      )}
+        </TouchableWithoutFeedback>
+      )
+      }
     </>
   );
 }
@@ -201,23 +295,45 @@ export default function BatchPay() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+  },
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 80,
+  },
+  mainText: {
+    fontSize: 20,
+    color: "black",
+    paddingBottom: 5,
+    paddingTop: 0,
+    marginTop: 0,
   },
   content: {
-    padding: 5
+    padding: 10,
+    marginTop: 90,
+    overflow: "scroll",
+    height: "50%",
+    borderWidth: "3px",
+    borderColor: "black",
+    borderStyle: "solid",
+    marginBottom: 0,
   },
   list: {
-    marginTop: '25%'
+    marginTop: 0,
+    marginBottom: 0,
+    paddingVertical: 0,
   },
-  flex: {
+  bottom: {
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
-    width: '100%',
     padding: 20,
-    marginBottom: 20
+    marginBottom: 50,
   },
   buttonStyle: {
-    backgroundColor: "#8B8B8B",
+    backgroundColor: "#3399FF",
     borderWidth: 0,
     color: "#FFFFFF",
     borderColor: "#3399FF",
@@ -226,16 +342,15 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     marginLeft: 35,
     marginRight: 35,
-    marginBottom: 20,
-    bottom: '7%',
-    width: '100%'
+    marginTop: 10,
+    marginBottom: 10,
   },
   buttonTextStyle: {
+    color: "#FFFFFF",
     paddingVertical: 10,
     paddingHorizontal: 15,
     fontSize: 16,
     fontWeight: "600",
-    fontFamily: "Helvetica Neue"
   },
   input: {
     height: 40,
